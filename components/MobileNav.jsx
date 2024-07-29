5035b5ad056c37be87a55b26621a28bb17ce82ba
Mahin Khan <md.mahin.bd18@gmail.com>
2024-07-29 21:46:00 +0600

    added sheet from shadecn

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,45 +1,68 @@
 "use client";
-import{Sheet,SheetContent, SheetTrigger} from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import {CiMenuFries} from 'react-icons/ci';
+import { CiMenuFries } from 'react-icons/ci';
 
-const links =[
-    {
-        name: "Home",
-        path: "/",
-      },
-      {
-        name: "Services",
-        path: "/services",
-      },
-      {
-        name: "Resume",
-        path: "/resume",
-      },
-      {
-        name: "Works",
-        path: "/works",
-      },
-      {
-        name: "Contact",
-        path: "/contact",
-      },
+const Links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Services",
+    path: "/services",
+  },
+  {
+    name: "Resume",
+    path: "/resume",
+  },
+  {
+    name: "Works",
+    path: "/works",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
 ];
 
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
 
-const MobileNav =()=>{
- const pathname = usePathname();
- return<Sheet>
-    <SheetTrigger className ="flex justify-center items-center">
-        <CiMenuFries className="text-[32px] text-accent"/>
-    </SheetTrigger>
-    
-    <SheetContent className="flex flex-col">
-        {/*Logo  */}
-    <div>Logo</div>
-    </SheetContent>
- </Sheet>
-}
+      <SheetContent className="flex flex-col">
+        {/* Logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Mahin<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* Nav */}
+        <nav className="flex flex-col justify-center gap-4">
+          {Links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`text-xl capitalize hover:text-accent transition-all ${
+                  link.path === pathname ? "text-accent border-b-2 border-accent" : ""
+                }`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default MobileNav; 
+export default MobileNav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,28 +11,12 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "15px",
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
-    fontFamily: {
-      primary: ["var(--font-jetbrainsMono)"],
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
-      colors: {
-        primary:  '#1c1c22',
-        accent:  {
-          DEFAULT: '#00ff99',
-          hover: '#00e187',
-
-        },
-
-      },
-
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
